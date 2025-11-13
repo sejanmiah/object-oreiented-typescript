3006cd6a861c8eb797076aafe2e04450ee481dda
Sejan Miah <sejankhan931@gmail.com>
2025-11-13 22:53:53 +0400

    here is gurd

--- src/typegourd.ts
+++ src/typegourd.ts
@@ -1,2 +1,16 @@
 // type Gurd
 
+const add = (num1: number | string, num2: number | string) => {
+
+    // Here is gurd 
+    if(typeof num1 === 'number' && typeof num2 === 'number'){
+        
+        return num1 + num2;
+    }
+    else{
+        num1.toString() + num2.toString();
+    }
+}
+
+add(2,2); //4
+add(2,'2') // 22
