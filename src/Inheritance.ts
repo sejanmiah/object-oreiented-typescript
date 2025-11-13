2e35c1e6aa000c00131555d0fb18f4754500257a
Sejan Miah <sejankhan931@gmail.com>
2025-11-13 22:17:03 +0400

    Inheritance check

--- src/Inheritance.ts
+++ src/Inheritance.ts
@@ -1,1 +1,24 @@
 // Inheritance, the 1st pilar of OOP
+
+class Student {
+    name: string;
+    age: number;
+    address: string
+
+    constructor(name: string, age: number, address:string) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+
+    getSleep(numOfHours: number){
+        console.log(`${this.name} ${numOfHours} Eni eto gonta ghumai`);
+        // console.log(`${this.name} ${numOfHours} ghonta ghumai`);
+    }
+}
+
+
+const student1 = new Student(`Mr. Fakibazz`, 18 ,"Bangladesh");
+
+student1.getSleep(15);
