d6e4ebf7567488b4cf9c3167fd1c265c72686f50
Sejan Miah <sejankhan931@gmail.com>
2025-11-13 21:31:51 +0400

    now added for cat

--- src/class.ts
+++ src/class.ts
@@ -11,10 +11,13 @@ class Animal {
 }
 
 
-const dog = new Animal('Dogesh bhai', 'dog', 'gheu gheu')
+const dog = new Animal('Dogesh bhai', 'dog', 'gheu gheu');
+
+const cat = new Animal('puci', 'cat', 'meu meu')
 
 
 console.log(dog.name)
+console.log(cat.sound)
 
 // function add (num1 : number, num2:number) {
 
